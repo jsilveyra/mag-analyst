84ce7361ab05d343d4387dc2624f7ff24e66ab25
peteanmarina <mariiizoee@gmail.com>
2026-05-11 02:34:28 -0300

    implement model + ODE solvers + hysteresis plot

---
+++ src/MagneticParameters/JA_ODE.m
@@ -0,0 +1,39 @@
+function dMdH = JA_ODE(H, M, p, delta)
+%JA_ODE Right-hand side of the Jiles-Atherton differential equation
+%
+%   dM/dH = num / den
+%
+%   num = k*delta*c*dManh_dHeff + (Manh - M)*deltaM
+%   den = k*delta - alpha*num
+%
+%   Inputs:
+%     H     : applied magnetic field [A/m]
+%     M     : magnetization [A/m]
+%     p     : struct with fields Ms, a, alpha, k, c
+%     delta : sign(dH), expected +1 or -1
+%
+%   Output:
+%     dMdH  : differential susceptibility dM/dH
+
+    required_fields = {'Ms', 'a', 'alpha', 'k', 'c'};
+    if ~isstruct(p) || ~all(isfield(p, required_fields))
+        error('JA_ODE:InvalidParams', ...
+            'Input p must be a struct with fields Ms, a, alpha, k, c.');
+    end
+
+    if ~(isscalar(delta) && (delta == 1 || delta == -1))
+        error('JA_ODE:InvalidDelta', 'delta must be +1 or -1.');
+    end
+
+    Heff = H + p.alpha .* M;
+    [Manh, dManh_dHeff] = anhysteretic(Heff, p);
+
+    deltaM = 0.5 .* (1 + sign(Manh - M) .* delta);
+
+    num = p.k .* delta .* p.c .* dManh_dHeff ...
+        + (Manh - M) .* deltaM;
+    den = p.k .* delta - p.alpha .* num;
+
+    dMdH = num ./ den;
+end
+
