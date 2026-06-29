c1551fb5b4048f184550c6067dfbe23c1ebdb8c7
jsilveyra <45441239+jsilveyra@users.noreply.github.com>
2026-06-29 11:32:25 -0300

    fix errors in unit conversions

--- src/Parser/UnitConvertor.m
+++ src/Parser/UnitConvertor.m
@@ -1,3 +1,10 @@
+% Constants:
+% mu0 = 4*pi*1e-7
+% 1 Oe = 1000/(4*pi) A/m = 79.5774715459 A/m
+% 1 emu/cm^3 = 1000 A/m
+% 1 T = 10000 G
+% 1 kG = 1000 G = 0.1 T
+
 classdef UnitConvertor
     properties
         UnitConversions
@@ -6,9 +13,10 @@ classdef UnitConvertor
     methods
         function obj = UnitConvertor()
             units = ["H [A/m]" "H [kA/m]" "H [Oe]" "H [kOe]" "Bext [T]" "Bext [G]" "Bext [kG]" "M [A/m]" "M [kA/m]" "M [MA/m]" "M [emu/cm^3]" "J [T]" "B [T]" "B [G]" "B [kG]"];
-            oe_conversion = 79.5774715459;
-            tesla_conversion = 1/(4*pi*10^-7);
-            gauss_conversion = 1/(10000 * tesla_conversion);
+            oe_conversion = 79.5774715459; % 1000/(4*pi)
+            mu0 = 4*pi*1e-7;
+            tesla_conversion = 1/mu0; % multiply T by this to get A/m
+            gauss_conversion = tesla_conversion * 1e-4; % 1 G = 1e-4 T
             conversion = [1 1000 oe_conversion oe_conversion*1000 tesla_conversion gauss_conversion gauss_conversion*1000 1 1000 1000000 1000 tesla_conversion tesla_conversion gauss_conversion gauss_conversion*1000];
             obj.UnitConversions = dictionary(units, conversion);
         end
